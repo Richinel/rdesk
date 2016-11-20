a93a1e3e593521691f585ee575079cb7dc60703a
DESKTOP-ELIGO1N\Richi <richinel_jarbandhan@hotmail.com>
2016-11-21 00:18:35 +0100

    Added database file

--- rdesk.sql
+++ rdesk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 17 nov 2016 om 01:15
+-- Gegenereerd op: 21 nov 2016 om 00:17
 -- Serverversie: 5.6.17
 -- PHP-versie: 5.5.12
 
@@ -30,25 +30,21 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) NOT NULL,
   `msg` text NOT NULL,
-  `priority` int(11) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(255) NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `priority` int(11) NOT NULL DEFAULT '1',
+  `postdate` int(50) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'openstaand',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ticket_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tickets`
 --
 
-INSERT INTO `tickets` (`ticket_id`, `subject`, `msg`, `priority`, `date`, `status`) VALUES
-(1, 'PC werkt niet', 'Hallo,\r\n\r\nMijn pc doet het niet meer. HELP!', 1, '2016-11-16 14:47:33', 'openstaand'),
-(2, 'Internet werkt niet?', 'Hallo! \r\nMijn WiFi doet raar. Ik heb het wel nodig!', 1, '2016-11-16 15:20:48', 'openstaand'),
-(3, 'test', 'test', 0, '2016-11-16 15:53:52', 'openstaand'),
-(4, 'Test ticket', 'Test LAAHN', 0, '2016-11-16 15:54:17', 'openstaand'),
-(8, 'Computer start niet meer op!', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 0, '2016-11-16 16:56:21', ''),
-(9, 'Printer werkt niet meer goed', '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"', 0, '2016-11-17 01:11:54', ''),
-(11, 'Scanner lijkt defect?', '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."', 0, '2016-11-17 01:14:08', ''),
-(12, 'Laptop gecrasht', '"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."', 0, '2016-11-17 01:15:25', 'openstaand');
+INSERT INTO `tickets` (`ticket_id`, `subject`, `msg`, `created_by`, `priority`, `postdate`, `status`, `deleted`) VALUES
+(28, 'Internet werkt niet?', 'Mijn internet roaming werkt niet in België en Frankrijk. Telefoon goed ingesteld. Simyo instellingen ook. Zelfs de apn van de telefoon van mijn partner overgenomen, die wel werkend internet heeft in deze landen via simyo sim only.\r\nWat kan ik nog doen ?\r\n\r\nGroeten,\r\nPieter', '2', 1, 1479610226, 'openstaand', 0),
+(29, 'Scanner lijkt defect?', 'Hallo,\r\n\r\nMijn scanner gaat wel aan, maar mijn computer herkent hem niet. Zou hier aub naar gekeken kunnen worden?\r\n\r\nMet vriendelijke groet,\r\nJan', '3', 1, 1479610902, 'openstaand', 0);
 
 -- --------------------------------------------------------
 
@@ -64,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
@@ -72,7 +68,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
 (1, 'Richinel', 'Jarbandhan', 'rj_1993@live.nl', '1234567', 3),
-(2, 'Rohit', 'Debidien', 'rohit1@live.nl', '1234567', 1);
+(2, 'Rohit', 'Debidien', 'rohit1@live.nl', '1234567', 1),
+(3, 'Jan', 'Zondergeld', 'j.zondergeld@gmail.com', '1234567', 1),
+(4, 'Dirk', 'Metgeld', 'd.metgeld@gmail.com', '1234567', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
